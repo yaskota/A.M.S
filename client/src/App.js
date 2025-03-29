22be53724db424b23dbe54b25a8ceadeea62346f
yaswanth kota <yaswanthkota3@gmail.com>
2025-03-29 21:18:06 +0530

    add

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Login from './components/Login.js';
 import Mail from './components/Mail.js';
 import Otp from './components/Otp.js';
 import Resetpassword from './components/Resetpassword.js';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route,Navigate } from 'react-router-dom';
 import Main from './components/Main.js';
 import Teachermain from './components/Teachermain.js';
 import Create_class from './components/Create_class.js';
@@ -16,7 +16,8 @@ function App() {
     <div className='App'>
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>} />
+        <Route path="/" element={<Navigate to="/login"/>} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Registration/>} />
         <Route path="/otp" element={<Otp/>} />
         <Route path="/mail" element={<Mail/>}/>
